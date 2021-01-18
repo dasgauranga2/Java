882474ea8f36b35021d2f85e233dd388966028c7
Gauranga Das <dasgauranga2@gmail.com>
2021-01-18 13:53:30 +0530

    Operators

---
+++ Operators/arithmetic_operators.java
@@ -0,0 +1,18 @@
+
+public class arithmetic_operators {
+    public static void main(String[] args) {
+        
+        int x = 10, y = 4;
+        int a = 13, b = 18;
+
+        // modulus operator returns the remainder of the division
+        System.out.println(x%y);
+
+        // increment operator increases the value by one
+        a++;
+        System.out.println(a);
+        // decrement operator decreases the value by one
+        b--;
+        System.out.println(b);
+    }
+}
---
+++ Operators/logical_operators.java
@@ -0,0 +1,15 @@
+
+public class logical_operators {
+    public static void main(String[] args) {
+        
+        boolean a = true;
+        boolean b = false;
+
+        // Logical AND operator
+        System.out.println(a && b);
+        // Logical OR operator
+        System.out.println(a || b);
+        // Logical NOT operator
+        System.out.println(!a);
+    }
+}
